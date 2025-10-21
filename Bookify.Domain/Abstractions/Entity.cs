7a3f1c2f13b46b0af7050bc1476cb57f4619e905
Moaaz Dandrawy <moaazdandrawy@gmail.com>
2025-10-21 22:08:57 +0300

    Add project files.

---
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookify.Domain.Abstractions
+{
+    public abstract class Entity
+    {
+        protected Entity(Guid id)
+        {
+            id = Id;
+        }
+        public Guid Id { get; init; }
+    }
+}
---
+++ Bookify.Domain/Apartment/Apartment.cs
@@ -0,0 +1,19 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Apartment
+{
+    public sealed class Apartment : Entity
+    {
+        public Apartment(Guid id) : base(id) { }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Address Address { get; private set; }
+        public decimal PriceAmount { get; private set; }
+        public string PriceCurrency { get; private set; }
+        public decimal CleaningFeeAmount { get; private set; }
+        public string CleaningFeeCurrency { get; private set; }
+        public DateTime LastBookedOnUtc { get; private set; }
+        public List<Amenity> Amenities { get; private set; } = new();
+
+    }
+}
