90b167af1e25362cdbafe440eb1149b47f4aed91
Moaaz Dandrawy <moaazdandrawy@gmail.com>
2025-10-26 11:58:39 +0300

    Adding IUnitOfWork & IRepos

---
+++ Bookify.Domain/Apartment/IApartmentRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Domain.Apartment
+{
+    public interface IApartmentRepository
+    {
+        Task<Apartment?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
+ 
