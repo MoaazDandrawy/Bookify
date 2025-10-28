3cdf5b0885e1c64dea63b7e771afb58f1ba9b11f
Moaaz Dandrawy <moaazdandrawy@gmail.com>
2025-10-28 12:20:39 +0300

    Review Entity

---
+++ Bookify.Domain/Reviews/Rating.cs
@@ -0,0 +1,23 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Reviews;
+
+public sealed record Rating
+{
+    //hoa 3aml el Rating Value Object 3lshan 5atr y7ot shwia business logic fiha zai en lazm el range ykoon mn 1 l 5
+    public static readonly Error Invalid = new("Rating.Invalid", "The rating is invalid");
+
+    private Rating(int value) => Value = value;
+
+    public int Value { get; init; }
+
+    public static Result<Rating> Create(int value)
+    {
+        if (value < 1 || value > 5)
+        {
+            return Result.Failure<Rating>(Invalid);
+        }
+
+        return new Rating(value);
+    }
+}
