049d12c9f30bbae01896c0083526feefe52602ae
Moaaz Dandrawy <moaazdandrawy@gmail.com>
2025-10-28 11:33:07 +0300

    Add Error & Result & Booking Business Logic & Booking Events

---
+++ Bookify.Domain/Abstractions/Result.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Bookify.Domain.Abstractions
+{
+    public class Result
+    {
+        protected internal Result(bool isSuccess, Error error)
+        {
+            // el conditions elgaya di 3lshan ymn3 en fih 7ala 8ariba yt3mlha create y3ny lazm tlama en IsSuccess b true tkon el Error b None w el3aks sa7i7
+            if (isSuccess && error != Error.None)
+            {
+                throw new InvalidOperationException();
+            }
+            if (!isSuccess && error == Error.None)
+            {
+                throw new InvalidOperationException();
+            }
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess; //property
+        public Error Error { get; }
+
+        //some helper static methods
+        public static Result Success() => new(true, Error.None);
+        public static Result Failure(Error error) => new(false, error);
+        public static Result<TValue> Success<TValue>(TValue value) => new(value, true, Error.None);
+        public static Result<TValue> Failure<TValue>(Error error) => new(default, false, error);
+
+        public static Result<TValue> Create<TValue>(TValue? value) =>
+            value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
+    }
+    //f el result 3amlna noo3 generic 3lshan lw el 3amlia bta3ty rag3t value zay getUserById el user hena lw rg3 hoa da el value lakn 7aga zai saveChanges() mfhash value f hanst5dm el noo3 el 3ady
+    public class Result<TValue> : Result
+    {
+        private readonly TValue? _value;
+
+        protected internal Result(TValue? value, bool isSuccess, Error error) : base(isSuccess, error)
+        {
+            _value = value;
+        }
+        [NotNull]
+        //hena enta t2dr twsl l el value only if en el result successed 8er keda mt2drsh twsl l el value
+        public TValue value => IsSuccess ? _value! : throw new InvalidOperationException("The value of a failure result can not be accessed.");
+
+        public static implicit operator Result<TValue>(TValue? value) => Create(value);
+    }
+}
---
+++ Bookify.Domain/Bookings/BookingErrors.cs
@@ -0,0 +1,28 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Bookings
+{
+    public static class BookingErrors
+    {
+        // el class da fih kol el errors elly momkn t7sl l el Booking Entity
+        public static Error NotFound = new(
+            "Booking.Found",
+            "The booking with the specified identifier was not found");
+
+        public static Error Overlap = new(
+            "Booking.Overlap",
+            "The current booking is overlapping with an existing one");
+
+        public static Error NotReserved = new(
+            "Booking.NotReserved",
+            "The booking is not pending");
+
+        public static Error NotConfirmed = new(
+            "Booking.NotReserved",
+            "The booking is not confirmed");
+
+        public static Error AlreadyStarted = new(
+            "Booking.AlreadyStarted",
+            "The booking has already started");
+    }
+}
