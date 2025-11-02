6878ea8f855ed4d17062658d101ddaa07f07be80
Moaaz Dandrawy <moaazdandrawy@gmail.com>
2025-11-02 13:22:42 +0200

    Get Booking By Id (Query)

---
+++ Bookify.Application/Abstractions/Data/ISqlConnectionFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace Bookify.Application.Abstractions.Data
+{
+    // i need a way to get a database connection so i can execute queries using dapper
+    public interface ISqlConnectionFactory
+    {
+        IDbConnection CreateConnection();//return new database connection to my sql DB w elly rag3 da momkn ykoon SqlConnection aw MySqlConnection 3la 7asb
+    }
+}
---
+++ Bookify.Application/Bookings/GetBooking/BookingResponse.cs
@@ -0,0 +1,32 @@
+﻿using Bookify.Domain.Bookings;
+using Bookify.Domain.Shared;
+
+namespace Bookify.Application.Bookings.GetBooking
+{
+    // el class da zai el booking blzabt bas bn5lih b el primative type mn 8er value objects
+    // da 3aml akno Dto w fih el props elly hatrg3 mn el DB
+    // kol el props hatkoon init setter
+    public sealed class BookingResponse
+    {
+        public Guid Id { get; init; }
+        public Guid ApartmentId { get; init; }
+        public Guid UserId { get; init; }
+        public DateOnly DurationStart { get; init; }
+        public DateOnly DurationEnd { get; init; }
+        public decimal PriceAmount { get; init; }
+        public string PriceCurrency { get; init; }
+        public decimal CleaningFeeAmount { get; init; }
+        public string CleaningFeeCurrency { get; init; }
+        public decimal AmenitiesUpChargeAmount { get; init; }
+        public string AmenitiesUpChargeCurrency{ get; init; }
+        public decimal TotalPriceAmount { get; init; }
+        public string TotalPriceCurrency { get; init; }
+        public int Status { get; init; }
+        public DateTime CreatedOnUtc { get; init; }
+
+        //public DateTime? ConfirmedOnUtc { get; init; }
+        //public DateTime? RejectedOnUtc { get; init; }
+        //public DateTime? CompletedOnUtc { get; init; }
+        //public DateTime? CancelledOnUtc { get; init; }
+    }
+}
