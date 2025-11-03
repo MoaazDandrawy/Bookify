7423f7c9ec67c60817b3a0a09eec0f19bc2899c9
Moaaz Dandrawy <moaazdandrawy@gmail.com>
2025-11-03 22:47:35 +0200

    Cross-Cutting Concerns (Logging)

---
+++ Bookify.Application/Abstractions/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,40 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Bookify.Application.Abstractions.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>// IPipeLineBehavior da 3lshan y2ool en el class da haikon part mn el MediatR PipeLine
+        where TRequest : IBaseCommand//howa 3aml implement l IBaseCommand 3lshan hai3ml logging l el Commands Bas
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public LoggingBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        //                                  el request da el Command                    next da el Command Handler 
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var name = request.GetType().Name;//bisagl esm el Command zai ReserveBookingCommand using Reflection
+
+            try
+            {
+                _logger.LogInformation("Executing command {Command}", name);
+
+                var result = await next();//hena binafz f3ln el Command zat nafso ya3ny lw ReserveBookingCommand haibtdy fiha
+
+                _logger.LogInformation("Command {Command} processed successfully", name);
+
+                return result;
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "Command {Command} processing failed", name);
+
+                throw;
+            }
+        }
+    }
+}
--- Bookify.Application/DependencyInjection.cs
+++ Bookify.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Bookify.Domain.Bookings;
+﻿using Bookify.Application.Abstractions.Behaviors;
+using Bookify.Domain.Bookings;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookify.Application
@@ -12,7 +13,9 @@ namespace Bookify.Application
             services.AddMediatR(configuration =>
             {
                 configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);//di bt3ml wiring up between command & command handlers and Query & Query Handlers.
-                // el line da 3lshan ysagl kol el handlers, behaviours automatic 3lshan a2dr ast5dm IMediator.Send() masln mn 8er ma asgl kol handler manual
+                // el line da (RegisterServicesFromAssembly) 3lshan ysagl kol el handlers, behaviours automatic 3lshan a2dr ast5dm IMediator.Send() masln mn 8er ma asgl kol handler manual
+
+                configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));//da hena 3lshan kol mara ykoon 3andy Command b ai noo3 request w response hai3dy 3la da
             });
             services.AddTransient<PricingService>();//3amel el noo3 Transient 3lshan kol mara ytlob el service y3ml mnha instance
             return services;
