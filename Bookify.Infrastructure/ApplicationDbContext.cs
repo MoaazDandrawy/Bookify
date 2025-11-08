99965c0a755fe66164b33f43debda07632ec6fe3
Moaaz Dandrawy <moaazdandrawy@gmail.com>
2025-11-08 13:52:50 +0200

    Add EF

---
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure
+{
+    public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        //IUnitOfWork goaha SaveChangesAsync w el mafrood yt3mlha Implement bas hia mawgoda already goa el DBContext 3lshan keda msh mdy error 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using Bookify.Application.Abstractions.Email;
 using Bookify.Infrastructure.Clock;
 using Bookify.Infrastructure.Email;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,6 +15,14 @@ namespace Bookify.Infrastructure
             services.AddTransient<IDateTimeProvider, DateTimeProvider>();
 
             services.AddTransient<IEmailService,EmailService>();
+
+            var connectionString = configuration.GetConnectionString("Database") ?? throw new ArgumentNullException(nameof(configuration));
+
+            services.AddDbContext<ApplicationDbContext>(options=>
+            {
+                options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();//PostgreSql
+                //we used snakeCase becuase Postgre Sql prefers it and the default of EF is TitleCase
+            });
 
             return services;
         }
