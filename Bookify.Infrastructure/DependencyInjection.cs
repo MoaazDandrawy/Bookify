78c1ff1999e1d0df1529b8f3cd88405bd26f940e
Moaaz Dandrawy <moaazdandrawy@gmail.com>
2025-11-09 13:16:15 +0200

    Define Repositories

--- Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,12 @@
 ﻿using Bookify.Application.Abstractions.Clock;
 using Bookify.Application.Abstractions.Email;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Users;
 using Bookify.Infrastructure.Clock;
 using Bookify.Infrastructure.Email;
+using Bookify.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,19 +15,24 @@ namespace Bookify.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddTransient<IDateTimeProvider, DateTimeProvider>();
 
-            services.AddTransient<IEmailService,EmailService>();
+            services.AddTransient<IEmailService, EmailService>();
 
             var connectionString = configuration.GetConnectionString("Database") ?? throw new ArgumentNullException(nameof(configuration));
 
-            services.AddDbContext<ApplicationDbContext>(options=>
+            services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();//PostgreSql
                 //we used snakeCase becuase Postgre Sql prefers it and the default of EF is TitleCase
             });
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IApartmentRepository, ApartmentRepository>();
+            services.AddScoped<IBookingRepository, BookingRepository>();
+
+            services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());//di registration l el Unit of work ya3ny lma binady IUnitOfWork biroo7 ydih nos5a mn El ApplicationDbContext
 
             return services;
         }
---
+++ Bookify.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,23 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure.Repositories
+{
+    internal abstract class Repository<T> where T : Entity
+    {
+        protected readonly ApplicationDbContext DbContext;
+
+        protected Repository(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+        public async Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await DbContext.Set<T>().FirstOrDefaultAsync(x => x.Id == id, cancellationToken);//el Set() di aknha btrag3 DbSet<Apartment>
+        }
+        public void Add(T Entity)
+        {
+            DbContext.Add(Entity);//hena e7na mst5dmnash Set<T> 3lshan hoa hai2dr y7dd el type mn el Entity
+        }
+    }
+}
